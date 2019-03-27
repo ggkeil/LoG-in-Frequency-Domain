40bc998db0b27869090b0ce54c66162c188d7366
ggkeil <ggkeil@cpp.edu>
2019-03-27 15:26:56 -0700

    Normalized the Gaussian Kernel and Added the Magnitude Plot For the Image

--- LoG_in_Freq_Dom.py
+++ LoG_in_Freq_Dom.py
@@ -20,11 +20,12 @@ def padImage(image):
                                 # P = 2M and Q = 2N
     return image
     
+# normalize the image so that pixel values [0, 1]
 def normalize(image):
     image = image // np.amax(image)
     return image
     
-# multiply by (-1)^(x+y)
+# multiply by (-1)^(x+y) to center or uncenter the image
 def center(image):
     x, y = image.shape[:2]
     center = np.zeros((x, y))
@@ -34,6 +35,7 @@ def center(image):
     centeredImage = image * center
     return centeredImage
 
+# calculate the gaussian kernel
 def gaussiankernel(image, sigma):
     iH = image.shape[0]
     iW = image.shape[1]
@@ -43,8 +45,10 @@ def gaussiankernel(image, sigma):
         for v in range(0, iW):
             D = np.sqrt((u - cu)**2 + (v - cv)**2)
             kernel[u, v] = np.exp(-(D**2) / (2 * (sigma**2)))
+    kernel = kernel // np.amax(kernel)
     return kernel
     
+# calculate the laplacian kernel
 def laplaciankernel(image):
     iH = image.shape[0]
     iW = image.shape[1]
@@ -54,11 +58,13 @@ def laplaciankernel(image):
         for v in range(0, iW):
             D = np.sqrt((u - cu)**2 + (v - cv)**2)
             kernel[u, v] = (-4)*((np.pi)**2)*(D**2)
-    kernel = kernel // np.amin(kernel)
+    kernel = kernel // np.amin(kernel) # normalize the Laplacian kernel
     return kernel
-    
-def multiply(fft, kernel1, kernel2):
-    output = fft*kernel1*kernel2
+
+# used to multiply a fourier image by two kernels  
+def multiply3(fft, kernel1, kernel2):
+    first = np.multiply(fft, kernel1)
+    output = np.multiply(first, kernel2)
     return output
 
 def multiply2(fft, kernel):
@@ -69,6 +75,15 @@ def calculateFFT(image):
     ftimage = np.fft.fft2(image)
     return ftimage
 
+# used for plotting the fourier of the image
+def plotMagnitudeSpectrum(image):
+    dft = cv2.dft(np.float32(image), flags = cv2.DFT_COMPLEX_OUTPUT)
+    dft_shift = np.fft.fftshift(dft)
+    
+    magnitude_spectrum = 20*np.log(cv2.magnitude(dft_shift[:,:,0], dft_shift[:,:,1]))
+    
+    return magnitude_spectrum
+
 # calculate IFFT after LoG
 def calculateIFFT(image):
     ifft = np.abs(np.fft.ifft2(image))
@@ -78,7 +93,7 @@ def calculateIFFT(image):
 def LoG(fft, sigma):
     gaussianKernel = gaussiankernel(fft, sigma) # first obtain the gaussian kernel calculated by this function
     laplacianKernel = laplaciankernel(fft) # then obtain the laplacian kernel calculated by this function
-    result = multiply(fft, gaussianKernel, laplacianKernel) # multiply the fourier transform by the gaussian and laplacian kernel
+    result = multiply3(fft, gaussianKernel, laplacianKernel) # multiply the fourier transform by the gaussian and laplacian kernel
     return result
 
 def unPad(paddedImage):
@@ -102,31 +117,25 @@ centered = center(normalized) # center the padded image
 
 fft = calculateFFT(centered) # calculate the fourier transform of the centered and normalized image
 
-gaussianFourier = multiply2(fft, gaussiankernel(fft, 25))
+magSpecPlot = plotMagnitudeSpectrum(padded) # plot the Magnitude Spectrum of the Padded Image
 
-gaussianIFFT = calculateIFFT(gaussianFourier)
+LoG = LoG(fft, 50) # find the Laplacian of Gaussian (LoG) of the padded, normalized, and centered image in fourier
 
-unCenteredGaussian = center(gaussianIFFT)
+ifft = calculateIFFT(LoG) # find the inverse fourier of the LoG
 
-unPaddedGaussian = unPad(unCenteredGaussian)
+unCenter = center(ifft) # uncenter the inverse fourier
 
-LoG = LoG(fft, 25)
+unPadded = unPad(unCenter) # unpad the uncentered inverse fourier
 
-ifft = calculateIFFT(LoG)
+sharpened = sharpen(gray, unPadded, -300) # sharpen the resulting image
 
-unCenter = center(ifft)
-
-unPadded = unPad(unCenter)
-
-sharpened = sharpen(gray, unPadded, 3)
-
-plt.subplot(121), plt.imshow(gray, cmap = 'gray')
+plt.subplot(221), plt.imshow(gray, cmap = 'gray') # plot the gray scale image
 plt.title('Input Image'), plt.xticks([]), plt.yticks([])
 
-#plt.subplot(222), plt.imshow(unPaddedGaussian, cmap = 'gray')
-#plt.title('Gaussian Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(222), plt.imshow(magSpecPlot, cmap = 'gray') # plot the magnitude spectrum
+plt.title('Magnitude Spectrum Plot of Padded Image'), plt.xticks([]), plt.yticks([])
 
-plt.subplot(122), plt.imshow(sharpened, cmap = 'gray')
+plt.subplot(223), plt.imshow(sharpened, cmap = 'gray') # plot the sharpened
 plt.title('Sharpened Image'), plt.xticks([]), plt.yticks([])
 
 plt.show()
